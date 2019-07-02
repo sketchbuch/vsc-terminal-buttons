8aa82ac5dbb3435a3586884eced2445b8d30c9ba
SketchBuch <sb.design.de@gmail.com>
2019-07-02 21:14:44 +0200

    chore: minor updates

--- src/test/utils/watch_terminals.test.ts
+++ src/test/utils/watch_terminals.test.ts
@@ -5,6 +5,11 @@ import createButtons from '../../utils/create_buttons';
 import mockButtons from '../mocks/mockButtons';
 import watchTerminals from '../../utils/watch_terminals';
 
+type SpiedActiveTerminal = sinon.SinonSpy<
+  [(e: vscode.Terminal | undefined) => any, any?, (vscode.Disposable[] | undefined)?],
+  vscode.Disposable
+>;
+
 type SpiedClosedTerminal = sinon.SinonSpy<
   [(e: vscode.Terminal) => any, any?, (vscode.Disposable[] | undefined)?],
   vscode.Disposable
@@ -16,13 +21,18 @@ type SpiedOpenTerminal = sinon.SinonSpy<
 >;
 
 suite('watchTerminals()', function() {
-  test('updateStatusbar() calls terminal open/close events', function() {
+  test('updateStatusbar() calls terminal open/close/change events', function() {
+    const spiedActiveTerminal: SpiedActiveTerminal = sinon.spy(vscode.window, 'onDidChangeActiveTerminal');
     const spiedCloseTerminal: SpiedClosedTerminal = sinon.spy(vscode.window, 'onDidCloseTerminal');
     const spiedOpenTerminal: SpiedOpenTerminal = sinon.spy(vscode.window, 'onDidOpenTerminal');
     const mockStatusButtons: vscode.StatusBarItem[] = createButtons(mockButtons);
+
     watchTerminals(mockStatusButtons);
+    assert(spiedActiveTerminal.called);
     assert(spiedCloseTerminal.called);
     assert(spiedOpenTerminal.called);
+
+    spiedActiveTerminal.restore();
     spiedCloseTerminal.restore();
     spiedOpenTerminal.restore();
   });
--- src/utils/watch_terminals.ts
+++ src/utils/watch_terminals.ts
@@ -1,21 +1,25 @@
 import * as vscode from 'vscode';
 import updateStatusbar from './update_statusbar';
 
+const terminalUpdate = (buttons: vscode.StatusBarItem[]): void => {
+  if (vscode.window.terminals.length > 0) {
+    updateStatusbar(vscode.window.activeTerminal, buttons);
+  } else {
+    updateStatusbar(undefined, buttons);
+  }
+};
+
 const watchTerminals = (buttons: vscode.StatusBarItem[]): void => {
+  vscode.window.onDidChangeActiveTerminal(() => {
+    terminalUpdate(buttons);
+  });
+
   vscode.window.onDidCloseTerminal(() => {
-    if (vscode.window.terminals.length > 0) {
-      updateStatusbar(vscode.window.activeTerminal, buttons);
-    } else {
-      updateStatusbar(undefined, buttons);
-    }
+    terminalUpdate(buttons);
   });
 
   vscode.window.onDidOpenTerminal(() => {
-    if (vscode.window.terminals.length > 0) {
-      updateStatusbar(vscode.window.activeTerminal, buttons);
-    } else {
-      updateStatusbar(undefined, buttons);
-    }
+    terminalUpdate(buttons);
   });
 };
 
